bfe0cc46a84b45dc4dbbc6449af974f101faf0df
Luis~ <luis_a_reyes13@hotmail.com>
2023-04-14 22:15:43 -0600

    Agregar archivos de proyecto.

---
+++ Textbox Dinamicos/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        LblX = New Label()
+        LblY = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(491, 51)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(369, 52)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(525, 121)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(17, 15)
+        Label1.TabIndex = 2
+        Label1.Text = "X:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(525, 146)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(17, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "Y:"
+        ' 
+        ' LblX
+        ' 
+        LblX.AutoSize = True
+        LblX.Location = New Point(549, 121)
+        LblX.Name = "LblX"
+        LblX.Size = New Size(37, 15)
+        LblX.TabIndex = 0
+        LblX.Text = "12345"
+        ' 
+        ' LblY
+        ' 
+        LblY.AutoSize = True
+        LblY.Location = New Point(549, 146)
+        LblY.Name = "LblY"
+        LblY.Size = New Size(37, 15)
+        LblY.TabIndex = 5
+        LblY.Text = "12345"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(696, 450)
+        Controls.Add(LblY)
+        Controls.Add(LblX)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LblX As Label
+    Friend WithEvents LblY As Label
+End Class
---
+++ Textbox Dinamicos/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim myPanel As Panel
+        myPanel = New Panel()
+        myPanel.BorderStyle = BorderStyle.FixedSingle
+        myPanel.Location = New Point(10, 10)
+        myPanel.Size = New Size(150, 500)
+        Me.Controls.Add(myPanel)
+
+        Dim xPos As Integer = 10
+        Dim yPos As Integer = 10
+        Dim textBoxesCount As Integer = Integer.Parse(TextBox1.Text)
+
+        For i As Integer = 1 To textBoxesCount
+            Dim myLabel As New Label()
+            myLabel.Location = New Point(xPos, yPos)
+            myLabel.Name = $"Lable_{i}"
+            myLabel.Text = $"Label {i}"
+            myLabel.AutoSize = True
+            myPanel.Controls.Add(myLabel)
+
+            Dim myTextBox As New TextBox()
+            myTextBox.Location = New Point(xPos, yPos + 20)
+            myTextBox.Name = $"Txt_{i}"
+            myPanel.Controls.Add(myTextBox)
+
+            yPos += 50
+        Next
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles MyBase.MouseMove
+        LblX.Text = e.X
+        LblY.Text = e.Y
+    End Sub
+End Class
